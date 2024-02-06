96a89376b3691513bc1bd31d026549b4f686d3c4
aosorioramirez <adrianosorioramirez@gmail.com>
2024-02-06 16:33:36 +0100

    Create first scene with Three.js
    He creado y preparado los archivos para tener una escena de prueba de Three.js

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
-// PRUEBA DEE EJERCICIO
+import {
+    Scene,
+    BoxGeometry,
+    MeshBasicMaterial,
+    Mesh,
+    PerspectiveCamera,
+    WebGLRenderer
+} from 'three';
+//1 The Scene
+const scene = new Scene();
+//2 The Geometry
+const geometry = new BoxGeometry(0.5,0.5,0.5);
+const material = new MeshBasicMaterial({color:'orange'});
+const mesh =new Mesh(geometry,material);
+scene.add(mesh);
+//3 The Camera
+const sizes = {
+    width: 800,
+    height: 600
+}
+const camera = new PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+//4 The Renderer
+const canvas = document.getElementById('three-canvas');
+const renderer = new WebGLRenderer({canvas:canvas});
+renderer.setSize(sizes.width,sizes.height);
+renderer.render(scene,camera);
+
+function animationCube() {
+    mesh.rotation.x += 0.01;
+    mesh.rotation.z += 0.01;
+    renderer.render(scene,camera);
+    requestAnimationFrame(animationCube);
+}
+animationCube();
+
 
 const index = document.querySelector(".buttonIndex");
 
