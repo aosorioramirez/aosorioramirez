ce4e47d69581c8fa626937489e802663ac6ca37b
aosorioramirez <adrianosorioramirez@gmail.com>
2024-02-07 12:41:49 +0100

    Camera-Controls

--- index.js
+++ index.js
@@ -4,44 +4,88 @@ import {
     MeshBasicMaterial,
     Mesh,
     PerspectiveCamera,
-    WebGLRenderer
+    WebGLRenderer,
+    MOUSE,
+    Vector2,
+    Vector3,
+    Vector4,
+    Quaternion,
+    Matrix4,
+    Spherical,
+    Box3,
+    Sphere,
+    Raycaster,
+    MathUtils,
+    Clock
 } from 'three';
 
-//1 The Scene
+import CameraControls from 'camera-controls';
+
+// 1 The Scene
+
 const scene = new Scene();
 const canvas = document.getElementById('three-canvas');
 
-//2 The Geometry
+// 2 The Geometry
+
 const geometry = new BoxGeometry(0.5,0.5,0.5);
-const material = new MeshBasicMaterial({color:'orange'});
+const material = new MeshBasicMaterial({color:'green'});
 const mesh = new Mesh(geometry,material);
 scene.add(mesh);
 
-//3 The Camera
+// 3 The Camera
+
 const camera = new PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight);
 camera.position.z = 3;
 scene.add(camera);
 
-//4 The Renderer
+// 4 The Renderer
+
 const renderer = new WebGLRenderer({canvas:canvas});
 renderer.setPixelRatio(Math.min(window.devicePixelRatio,2));
 renderer.setSize(canvas.clientWidth,canvas.clientHeight,false);
 
-//5 Responsivity
+// 5 Responsivity
+
 window.addEventListener('resize', () => {
     camera.aspect = canvas.clientWidth / canvas.clientHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
-})
+});
 
-//6 Animation
-function animationCube() {
-    mesh.rotation.x += 0.01;
-    mesh.rotation.z += 0.01;
-    renderer.render(scene,camera);
-    requestAnimationFrame(animationCube);
+// 6 Camera-Controls
+
+const subsetOfTHREE = {
+    MOUSE,
+    Vector2,
+    Vector3,
+    Vector4,
+    Quaternion,
+    Matrix4,
+    Spherical,
+    Box3,
+    Sphere,
+    Raycaster,
+    MathUtils: {
+      DEG2RAD: MathUtils.DEG2RAD,
+      clamp: MathUtils.clamp
+    }
+};
+
+CameraControls.install( { THREE: subsetOfTHREE } );
+const clock = new Clock();
+const cameraControls = new CameraControls(camera, canvas);
+cameraControls.dollyToCursor = true;
+
+// 7 Animation
+
+function animate() {
+    const delta = clock.getDelta();
+    cameraControls.update( delta );
+    renderer.render( scene, camera );
+    requestAnimationFrame(animate);
 }
-animationCube();
+animate();
 
 
 const index = document.querySelector(".buttonIndex");
