d34ef660c998ab07f4c5ec103fa2d3c4a21f81f6
aosorioramirez <adrianosorioramirez@gmail.com>
2024-04-23 13:27:43 +0200

    v8

--- info.js
+++ info.js
@@ -5,7 +5,7 @@ export const nombresEspecialidades = [
     {'name': "GRASSHOPPER", 'icon': "🦗", 'url': "indexcarta.html"},
     {'name': "DYNAMO", 'icon': "⚙️", 'url': "indexcarta.html"},
     {'name': "PROGRAMACIÓN", 'icon': "💻", 'url': "indexcarta.html"},
-    {'name': "TFM: ESTRUCTURAS-RHINO.INSIDE", 'icon': "🌉", 'url': "indexcarta.html"}
+    {'name': "TFM: STRUCTURES RHINO.INSIDE", 'icon': "🌉", 'url': "indexcarta.html"}
 ]
 
 export const nombresProyectos = [
@@ -13,14 +13,14 @@ export const nombresProyectos = [
     {'name': "TFM: ESTRUCTURAS HINCHABLES",'number': "2", 'loader': "3dm loader/3dm-viewer", 'model': "Hinchable.3dm"},
     {'name': "TFM: PARABOLOIDES HORMIGÓN",'number': "3", 'loader': "3dm loader/3dm-viewer", 'model': "Cascarones.3dm"},
     {'name': "VIU I CONVIU",'number': "4", 'loader': "ifc loader/ifc-viewer", 'model': "ARQ.ifc"},
-    {'name': "VIVIENDA AZOTEA",'number': "5", 'loader': "ifc loader/ifc-viewer", 'model': "2203_BAS_v2_r23.ifc"},
+    {'name': "VVDA AZOTEA",'number': "5", 'loader': "ifc loader/ifc-viewer", 'model': "2203_BAS_v2_r23.ifc"},
+    {'name': "VVDA CURSO IFC JS",'number': "6", 'loader': "ifc loader/ifc-viewer", 'model': "01.ifc"},
+    {'name': "VIU I CONVIU",'number': "4", 'loader': "ifc loader/ifc-viewer", 'model': "ARQ.ifc"},
+    {'name': "VVDA AZOTEA",'number': "5", 'loader': "ifc loader/ifc-viewer", 'model': "2203_BAS_v2_r23.ifc"},
+    {'name': "VIVIENDA CURSO IFC JS",'number': "6", 'loader': "ifc loader/ifc-viewer", 'model': "01.ifc"},
+    {'name': "VVDA AZOTEA",'number': "5", 'loader': "ifc loader/ifc-viewer", 'model': "2203_BAS_v2_r23.ifc"},
     {'name': "VIVIENDA CURSO IFC JS",'number': "6", 'loader': "ifc loader/ifc-viewer", 'model': "01.ifc"},
     {'name': "VIU I CONVIU",'number': "4", 'loader': "ifc loader/ifc-viewer", 'model': "ARQ.ifc"},
-    {'name': "VIVIENDA AZOTEA",'number': "5", 'loader': "ifc loader/ifc-viewer", 'model': "2203_BAS_v2_r23.ifc"},
-    {'name': "VIVIENDA CURSO IFC JS",'number': "6", 'loader': "ifc loader/ifc-viewer", 'model': "01.ifc"},
-    {'name': "VIVIENDA AZOTEA",'number': "5", 'loader': "ifc loader/ifc-viewer", 'model': "2203_BAS_v2_r23.ifc"},
-    {'name': "VIVIENDA CURSO IFC JS",'number': "6", 'loader': "ifc loader/ifc-viewer", 'model': "01.ifc"},
-    {'name': "VIU I CONVIU",'number': "4", 'loader': "ifc loader/ifc-viewer", 'model': "ARQ.ifc"},
-    {'name': "VIVIENDA AZOTEA",'number': "5", 'loader': "ifc loader/ifc-viewer", 'model': "2203_BAS_v2_r23.ifc"},
+    {'name': "VVDA AZOTEA",'number': "5", 'loader': "ifc loader/ifc-viewer", 'model': "2203_BAS_v2_r23.ifc"},
     {'name': "VIVIENDA CURSO IFC JS",'number': "6", 'loader': "ifc loader/ifc-viewer", 'model': "01.ifc"}
 ];
