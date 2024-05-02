f53da4066da0c607e86a7383ac10f99c7d34bd60
aosorioramirez <adrianosorioramirez@gmail.com>
2024-05-02 03:09:33 +0200

    v12

--- curriculum/curriculum.js
+++ curriculum/curriculum.js
@@ -5,9 +5,28 @@ const imgFullPage = document.getElementById('imageFullPage');
 
 imgs.forEach(img => {
   img.addEventListener('click', function() {
-    imgFullPage.style.content = 'url(' + img.src + ')';
+    let innerH = window.innerHeight;
+    let innerW = window.innerWidth;
+
     fullPage.style.display = 'flex';
     fullPage.style.top = window.scrollY+'px';
+    imgFullPage.style.content = 'url(' + img.src + ')';
+
+    if (innerW > innerH && img.width < img.height) {
+      imgFullPage.style.height = '10%';
+    } 
+      else if (innerW > innerH && img.width > img.height) {
+        imgFullPage.style.width = '80%';
+        imgFullPage.style.height = '90%';
+    }
+      else if (innerW < innerH && img.width < img.height) {
+      imgFullPage.style.width = '80%';
+      imgFullPage.style.height = '80%';
+    } 
+      else {
+      imgFullPage.style.width = '90%';
+      imgFullPage.style.height = '50%';
+    };
 
     document.body.classList.add("stop-scrolling");
   });
