afa0bd1f2300a7813eb3a2f4fd0ada438d2dc98c
aosorioramirez <adrianosorioramirez@gmail.com>
2024-05-24 13:30:03 +0200

    Intento de finalización 1

--- info.js
+++ info.js
@@ -1,13 +1,14 @@
 export const nombresEspecialidades = [
-    {'name': "CONOCIMIENTOS E INTERESES", 'icon':  "🧠", 'url': "./curriculum/conocimientos-e-intereses.html", 'side': "left"},
-    {'name': "XX SOBRE MÍ", 'icon': "🐻​", 'url': "./curriculum/sobre-mi.html", 'side': "right"},
-    {'name': "XX BIM", 'icon':  "🛠️", 'url': "./curriculum/bim.html", 'side': "left"},
-    {'name': "XX IMPRESIÓN 3D", 'icon':  "🧵", 'url': "./curriculum/impresion-3d.html", 'side': "right"},
-    {'name': "XX GRASSHOPPER", 'icon': "🦗", 'url': "./curriculum/grasshopper.html", 'side': "left"},
+    {'name': "SOBRE MÍ", 'icon': "🐻​", 'url': "./curriculum/sobre-mi.html", 'side': "left"},
+    {'name': "PROGRAMACIÓN", 'icon': "💻", 'url': "./curriculum/programacion.html", 'side': "right"},
+    {'name': "BIM", 'icon':  "🛠️", 'url': "./curriculum/bim.html", 'side': "left"},
+    {'name': "IMPRESIÓN 3D", 'icon':  "🧵", 'url': "./curriculum/impresion-3d.html", 'side': "right"},
+    {'name': "GRASSHOPPER", 'icon': "🦗", 'url': "./curriculum/grasshopper.html", 'side': "left"},
     {'name': "DYNAMO", 'icon': "⚙️", 'url': "./curriculum/dynamo.html", 'side': "right"},
-    {'name': "XX PROGRAMACIÓN", 'icon': "💻", 'url': "./curriculum/programacion.html", 'side': "left"},
-    {'name': "XX TFM: STRUCTURES RHINO.INSIDE", 'icon': "🌉", 'url': "./curriculum/tfm.html", 'side': "right"}
+    {'name': "PFC (EN PROCESO)", 'icon': "❄️​", 'url': "", 'side': "left"},
+    {'name': "TFM: STRUCTURES RHINO.INSIDE", 'icon': "🌉", 'url': "./curriculum/tfm.html", 'side': "right"}
 ]
+
 
 export const nombresProyectos = [
     {'name': "TFM: ESTRUCTURAS TENSEGRITY",'number': "1", 'loader': "3dm loader/3dm-viewer", 'model': "Tensegrity.3dm"},
@@ -16,12 +17,4 @@ export const nombresProyectos = [
     {'name': "VIU I CONVIU",'number': "4", 'loader': "ifc loader/ifc-viewer", 'model': "ARQ.ifc"},
     {'name': "VIVIENDA AZOTEA",'number': "5", 'loader': "ifc loader/ifc-viewer", 'model': "2203_BAS_v2_r23.ifc"},
     {'name': "VIVIENDA CURSO IFC JS",'number': "6", 'loader': "ifc loader/ifc-viewer", 'model': "01.ifc"},
-    {'name': "VIU I CONVIU",'number': "4", 'loader': "ifc loader/ifc-viewer", 'model': "ARQ.ifc"},
-    {'name': "VIVIENDA AZOTEA",'number': "5", 'loader': "ifc loader/ifc-viewer", 'model': "2203_BAS_v2_r23.ifc"},
-    {'name': "VIVIENDA CURSO IFC JS",'number': "6", 'loader': "ifc loader/ifc-viewer", 'model': "01.ifc"},
-    {'name': "VIVIENDA AZOTEA",'number': "5", 'loader': "ifc loader/ifc-viewer", 'model': "2203_BAS_v2_r23.ifc"},
-    {'name': "VIVIENDA CURSO IFC JS",'number': "6", 'loader': "ifc loader/ifc-viewer", 'model': "01.ifc"},
-    {'name': "VIU I CONVIU",'number': "4", 'loader': "ifc loader/ifc-viewer", 'model': "ARQ.ifc"},
-    {'name': "VIVIENDA AZOTEA",'number': "5", 'loader': "ifc loader/ifc-viewer", 'model': "2203_BAS_v2_r23.ifc"},
-    {'name': "VIVIENDA CURSO IFC JS",'number': "6", 'loader': "ifc loader/ifc-viewer", 'model': "01.ifc"}
 ];
