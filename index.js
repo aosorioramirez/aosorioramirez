d0a3882967698047ecf3282a1cc605656636023b
aosorioramirez <adrianosorioramirez@gmail.com>
2024-06-04 13:25:58 +0200

    Prueba .htaccess 3

--- index.js
+++ index.js
@@ -101,6 +101,7 @@ for (const nombre of nombresEspecialidades) {
   text.onclick = function especialidadClick() {
     // sleep(500).then(() => location.href = nombre.url);
     sleep(300).then(() => location.href = nombre.url);
+    console.log(nombre.url);
   }
 
 
