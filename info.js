a65ee966281a8c1952a4c0a59b7a6d373e9a86bd
aosorioramirez <adrianosorioramirez@gmail.com>
2024-06-04 13:57:52 +0200

    Prueba .htaccess 6

--- info.js
+++ info.js
@@ -11,11 +11,11 @@ export const nombresEspecialidades = [
 
 
 export const nombresProyectos = [
-    {'name': "TFM: ESTRUCTURAS TENSEGRITY",'number': "1", 'loader': "3dm loader/3dm-viewer", 'model': "Tensegrity.3dm"},
-    {'name': "TFM: ESTRUCTURAS HINCHABLES",'number': "2", 'loader': "3dm loader/3dm-viewer", 'model': "Hinchable.3dm"},
-    {'name': "TFM: PARABOLOIDES HORMIGÓN",'number': "3", 'loader': "3dm loader/3dm-viewer", 'model': "Cascarones.3dm"},
-    // {'name': "VIU I CONVIU",'number': "4", 'loader': "ifc loader/ifc-viewer", 'model': "ARQ.ifc"},
-    {'name': "VIVIENDA AZOTEA",'number': "5", 'loader': "ifc loader/ifc-viewer", 'model': "2203_BAS_v2_r23.ifc"},
-    {'name': "VIVIENDA CURSO IFC JS",'number': "6", 'loader': "ifc loader/ifc-viewer", 'model': "01.ifc"},
-    {'name': "SOLEAMIENTO VALENCIA",'number': "7", 'loader': "sun loader/sun-viewer", 'model': "sun_valencia.3dm"},
+    {'name': "TFM: ESTRUCTURAS TENSEGRITY",'number': "1", 'loader': "3dm_loader/3dm-viewer", 'model': "Tensegrity.3dm"},
+    {'name': "TFM: ESTRUCTURAS HINCHABLES",'number': "2", 'loader': "3dm_loader/3dm-viewer", 'model': "Hinchable.3dm"},
+    {'name': "TFM: PARABOLOIDES HORMIGÓN",'number': "3", 'loader': "3dm_loader/3dm-viewer", 'model': "Cascarones.3dm"},
+    // {'name': "VIU I CONVIU",'number': "4", 'loader': "ifc_loader/ifc-viewer", 'model': "ARQ.ifc"},
+    {'name': "VIVIENDA AZOTEA",'number': "5", 'loader': "ifc_loader/ifc-viewer", 'model': "2203_BAS_v2_r23.ifc"},
+    {'name': "VIVIENDA CURSO IFC JS",'number': "6", 'loader': "ifc_loader/ifc-viewer", 'model': "01.ifc"},
+    {'name': "SOLEAMIENTO VALENCIA",'number': "7", 'loader': "sun_loader/sun-viewer", 'model': "sun_valencia.3dm"},
 ];
